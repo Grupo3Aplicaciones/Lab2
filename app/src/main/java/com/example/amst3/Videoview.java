6687bbfd573e097d780f84bb5eb343968bfe6cd4
carftorr <73551328+carftorr@users.noreply.github.com>
2020-11-08 16:45:59 -0500

    Video View

---
+++ app/src/main/java/com/example/amst3/Videoview.java
@@ -0,0 +1,80 @@
+package com.example.amst3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Videoview extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener,YouTubePlayer.PlaybackEventListener {
+
+    String claveYoutube="AIzaSyDzewW_Xl3xRl3Ve2egw1aTHFksdSuddyY";
+    YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtube_view);
+        youTubePlayerView.initialize(claveYoutube, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean fueRestaurado) {
+        if(!fueRestaurado){
+            youTubePlayer.cueVideo("t0qpvmNo3KE");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this, 1).show();
+        }else{
+            String error="Error al iniciar Youtube"+youTubeInitializationResult.toString();
+
+            Toast.makeText(getApplication(),error,Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultcode, Intent data) {
+
+        if (resultcode == 1) {
+            getYoutubePlayerProvider().initialize(claveYoutube, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYoutubePlayerProvider(){
+        return youTubePlayerView;
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+
+    }
+}
+
